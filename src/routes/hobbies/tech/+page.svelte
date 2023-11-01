b1905ea10d486d9ca8e709851f15687b399d9efb
Martin Havala <martin.havala@gmail.com>
2023-11-01 12:43:49 +0100

    Add Wedding Guests to projects

--- src/routes/hobbies/tech/+page.svelte
+++ src/routes/hobbies/tech/+page.svelte
@@ -1,2 +1,5 @@
-<a href="https://martin-havala.github.io/cheesecake-by-mona/">CheeseCake by Mona</a>
-<a href="https://martin-havala.github.io/check-that-mountains">Check those Mountains</a>
+<ul>
+    <li><a href="https://martin-havala.github.io/cheesecake-by-mona/">CheeseCake by Mona</a></li>
+    <li><a href="https://martin-havala.github.io/check-that-mountains">Check those Mountains</a></li>
+    <li><a href="https://martin-havala.github.io/wedding_guests/#/wedding_guests">Wedding Guests</a></li>
+</ul>
