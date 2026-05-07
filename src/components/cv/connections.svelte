ea12a0cc60f6b6a7711a8124731781f9f9597b86
Martin Havala <martin-havala@smarthead.com>
2026-05-07 09:47:26 +0200

    Update linkedin url

--- src/components/cv/connections.svelte
+++ src/components/cv/connections.svelte
@@ -1,52 +1,51 @@
 <div class="profile">
-    
-    <h1>Martin Havala</h1>
-    <div class="networks">
-        <a href="https://twitter.com/sawio" target="_parent">twitter</a>
-        <a href="https://github.com/martin-havala" target="_parent">github</a>
-        <a href="https://www.linkedin.com/in/martin-havala-4690b5214" target="_parent">linkedin</a>
-    </div>
-    <a class="meet"  target="_blank" href="https://calendar.app.google/afBossnqRszAmqmU8" title="Opens Google-Meet calendar in a new tab">meet Martin :)</a>
-    <!-- <img src="{base}/images/profile_pic.jpg" alt="Profile picture" aria-hidden="true" /> -->
+  <h1>Martin Havala</h1>
+  <div class="networks">
+    <a href="https://github.com/martin-havala" target="_parent">github</a>
+    <a href="https://www.linkedin.com/in/martin-havala" target="_parent"
+      >linkedin</a
+    >
+  </div>
+  <a
+    class="meet"
+    target="_blank"
+    href="https://calendar.app.google/afBossnqRszAmqmU8"
+    title="Opens Google-Meet calendar in a new tab">meet Martin :)</a
+  >
+  <!-- <img src="{base}/images/profile_pic.jpg" alt="Profile picture" aria-hidden="true" /> -->
 </div>
 
 <style lang="scss">
-    section {
-        h1 {
-            margin: 0;
-        }
+  section {
+    h1 {
+      margin: 0;
     }
-    .profile {
-        margin: 0 1em;
-        display: flex;
-        justify-content: start;
-        align-items: center;
-        // img {
-        //     width: 5em;
-        //     border-radius: 50%;
-        //     padding-right: 1em;
-        //     margin-left: 3em;
-        //     position: relative;
-        //     height: 5em;
-        // }
-    }
-    .networks {
-        font-size: 0.8em;
-        margin-left: 3em;
-    }
-    a {
-        padding: 0.5ex;
-    }
+  }
+  .profile {
+    margin: 0 1em;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+  }
+  .networks {
+    font-size: 0.8em;
+    margin-left: 3em;
+  }
+  a {
+    padding: 0.5ex;
+  }
 
-    .meet{
-        display: inline-block;
-        text-decoration: none;
-        background: var(--fg-color);
-        color: var(--bg-color);
-        border-radius: 1rem;
-        padding:.5rem 1rem;
-        margin-left:2rem;
-        
-        &:hover{background: var(--active-color);}
+  .meet {
+    display: inline-block;
+    text-decoration: none;
+    background: var(--fg-color);
+    color: var(--bg-color);
+    border-radius: 1rem;
+    padding: 0.5rem 1rem;
+    margin-left: 2rem;
+
+    &:hover {
+      background: var(--active-color);
     }
+  }
 </style>
